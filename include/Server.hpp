f10ad4d87749397be3a4c72a3fb13e5eb2c79839
Zakaria Bakour <zbakour@student.1337.ma>
2026-02-28 17:14:39 +0000

    resolved merge conflict

--- include/Server.hpp
+++ include/Server.hpp
@@ -3,12 +3,9 @@
 # include <Request/Request.hpp>
 # include <CGI/Cgi.hpp>
 # include <Response/Response.hpp>
-<<<<<<< HEAD
 
 #include <Parser/HTTP/Lexer.hpp>
 #include <Parser/HTTP/HttpParser.hpp>
-=======
-# include <Parser/Parser.hpp>
 # include <iostream>
 # include <errno.h>
 # include <cstddef>
@@ -17,4 +14,3 @@
 # include <string.h>
 # include <fcntl.h>
 # include <utility>
->>>>>>> origin/master
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-#include <Server.hpp>
-
-
-
-int main()
-{
-    HttpParser http;
-=======
 # include <Server.hpp>
 
 int main() {	 
 	Multiplexer::init();
 	Multiplexer::loop();
 	Multiplexer::deinit();
->>>>>>> origin/master
     return 0;
 }
