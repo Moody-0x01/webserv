3d135e1bfc16d0eb02572a23fbf969a468cf376e
st-ameen <M136332299@taalim.ma>
2026-04-09 12:05:43 +0100

    Quick fix

--- src/HTTP/Response.cpp
+++ src/HTTP/Response.cpp
@@ -272,12 +272,20 @@ bool Response::is_method_allowed(std::string method)
 	/*  std::cout << "Method: " << method;  */
 	/*  std::cout << "Allowed: " << resolved_results.matched_location;  */
 
+	if (!resolved_results.matched_location || resolved_results.matched_location->methods.empty())
+	{
+		if (method == "GET" || method == "POST" || method == "DELETE")
+			return (true);
+		else
+			return false;
+	}
+
 	for (size_t i = 0; i < resolved_results.matched_location->methods.size(); ++i)
 	{
 		if (resolved_results.matched_location->methods[i] == method)
 			return (true);
 	}
-	return (true);
+	return (false);
 }
 
 void Response::setup_response(const HttpRequest &request)
