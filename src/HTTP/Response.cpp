11e3a8d4856f029e1603f705cdbc3fe503e1ec1e
Lahoucine Azmoud <lazmoud@e2r6p6.1337.ma>
2026-05-13 17:49:09 +0100

    Ran the tester and found a bug.. the bug is in `./Test/testlogger.txt`

--- src/HTTP/Response.cpp
+++ src/HTTP/Response.cpp
@@ -109,7 +109,7 @@ static std::string build_filesystem_target(const UriResolutionResult &resolved, 
 	{
 		bool needs_index = false;
 		if (relative_uri == "/") needs_index = true;
-		else if (!resolved.request_path.empty() && resolved.request_path[resolved.request_path.size() - 1] == '/')
+		else if (!resolved.request_path.empty() && resolved.resource_type == UriResolutionResult::directory)
 			needs_index = true;
 		if (needs_index && request.method == "GET")
 			filesystem_path = join_fs_path(filesystem_path, resolved.index);
@@ -397,6 +397,8 @@ void Response::setup_response(const HttpRequest &request)
 		return ;
 	}
 	this->resolved_results = Response::resolve_uri_to_path(request);
+	std::cout << "fs path: " << this->resolved_results.filesystem_path << std::endl;
+	std::cout << "Index: "   << this->resolved_results.index           << std::endl;
 	if (this->resolved_results.resource_type == UriResolutionResult::redirect)
 	{
 		this->appendheader("Location", this->resolved_results.matched_location->return_loc.second.c_str());
